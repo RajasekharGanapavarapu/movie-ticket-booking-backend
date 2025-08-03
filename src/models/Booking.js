1ecb9ae33dc20953d2de647eff732db7f134d99d
RajasekharGanapavarapu <rajasekhar.ganapavarapu.18@gmail.com>
2025-08-03 17:24:48 +0530

    Create Booking.js

---
+++ src/models/Booking.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const bookingSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    show: { type: mongoose.Schema.Types.ObjectId, ref: 'Show', required: true },
+    seats: [String],
+    bookedAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Booking', bookingSchema);
